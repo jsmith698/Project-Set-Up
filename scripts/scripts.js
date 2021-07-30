e3e2c26e2dcd5be9fdf98aff6d3010e301fb0090
jsmith698 <85500280+jsmith698@users.noreply.github.com>
2021-07-30 07:14:45 -0500

    Delete scripts directory

--- scripts/scripts.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    console.log("Welcome to my Profile!!!");
-    //var el = document.getElementById("intro");
-    //el.innerText = "Welcome to my Profile";
-    $("#intro").text("Welcome to my Profile!!!");
-  });
